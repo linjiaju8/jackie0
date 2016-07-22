a7fc1b66d978da8fbafccaca1c1512a3427331a8
无形无常 <linjiaju8@163.com>
2016-07-22 17:59:00 +0800

    把平台的日志打印方式又log4j2改为logback，因为log4j2存在兼容性问题

--- platform/src/main/java/com/jackie0/platform/PlatformApplication.java
+++ platform/src/main/java/com/jackie0/platform/PlatformApplication.java
@@ -7,10 +7,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan
